a7b3945443ef82599a2624803921c2cda1498426
Kaung Myat Tun <kaungtun111@gmail.com>
2022-08-13 22:36:05 +0630

    CRUD operations

--- src/App.js
+++ src/App.js
@@ -7,20 +7,57 @@ import PostPage from './PostPage'
 import About from './About'
 import Missing from './Missing'
 import React from 'react'
-import { Route, Routes, useHistory } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 
 function App() {
+
+  const [posts, setPosts] = useState([
+      {
+        id: 1,
+        title: "My First Post",
+        datetime: "July 01, 2021 11:17:36 AM",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+      },
+      {
+        id: 2,
+        title: "My 2nd Post",
+        datetime: "July 01, 2021 11:17:36 AM",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+      },
+      {
+        id: 3,
+        title: "My 3rd Post",
+        datetime: "July 01, 2021 11:17:36 AM",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+      },
+      {
+        id: 4,
+        title: "My Fourth Post",
+        datetime: "July 01, 2021 11:17:36 AM",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+      }
+  ])
+  const [search, setSearch] = useState('')
+  const [searchResults, setSearchResults] = useState([])
+  const navigate = useNavigate()
+
+  const handleDelete = id => {
+      const postList = posts.filter(post => post.id !== id)
+      setPosts(postList)
+      navigate('/')
+  }
+
   return (
     <div className="App">
         <Header title="React JS Blog" />
-        <Nav />
+        <Nav search={search} setSearch={setSearch} />
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<Home posts={posts} />} />
 
             <Route path='/post' element={<NewPost />} />
             
-            <Route path='/post/:id' element={<PostPage />} />
+            <Route path='/post/:id' element={<PostPage posts={posts} handleDelete={handleDelete}/>} />
             
             <Route path='/about' element={<About />} />
             
